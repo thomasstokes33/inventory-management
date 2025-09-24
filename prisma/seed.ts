4b8b1f43be5ab6d0270568ab1bf9fc0ff8f16fa4
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-09-24 18:13:06 +0100

    Improve the database seed.
    (Mainly to get to grips with prisma creation and access.)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,24 +3,43 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const sampleSupplier: Prisma.SupplierCreateInput = {
+const sampleSuppliers: Prisma.SupplierCreateInput[] = [{
     name: "Baking Inc",
     email: "mary@backinginc.com"
-};
+},
+{
+    name: "Chems R Us",
+    email: "jane@chemsrus.in"
+}
+];
 
+
+const hazardClasses: Prisma.HazardClassCreateInput[] = [
+    { classification: "Explosives" },
+    { classification: "Gases" },
+    { classification: "Flammable liquid" },
+    { classification: "Flammable solids" },
+    { classification: "Oxidising substances" },
+    { classification: "Toxic substances" },
+    { classification: "Radioactive material" },
+    { classification: "Corrosive substances" },
+    { classification: "Miscellaneous dangerous goods" }
+];
 export async function main() {
-    const newSupplier: {
-        name: string;
-        email: string;
-        id: number;
-    } = await prisma.supplier.create({ data: sampleSupplier });
-
+    const supplierMap : Map<string, number> = new Map();
+    for (const haz of hazardClasses) {
+        await prisma.hazardClass.create({ data: haz });
+    }
+    for (const supplier of sampleSuppliers) {
+        const addedSupplier = await prisma.supplier.create({ data: supplier });
+        supplierMap.set(addedSupplier.name, addedSupplier.id);
+    }
     const newChemicals: Prisma.ChemicalCreateInput[] = [{
         name: "vanilla extract",
-        stock_quantity: 19.8,
-        unit: "ml",
+        stockQuantity: 19.8,
+        quantityType: "VOLUME",
         status: "APPROVED",
-        supplier: { connect: { id: newSupplier.id } }
+        supplier: { connect: {  id: supplierMap.get(sampleSuppliers[0].name)} }
     }];
     for (const u of newChemicals) {
         await prisma.chemical.create({
