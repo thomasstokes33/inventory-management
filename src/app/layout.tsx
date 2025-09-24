06bf11e45f719a2b334c94971561a6144e0e6ba9
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-09-24 18:14:57 +0100

    Add bootstrap css via CDN.
    This is just for the mvp and hopefully this project will migrate to tailwind.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <head>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossOrigin="anonymous"/>
+        </head>
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
