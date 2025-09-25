dd0c81033d3226209e549dfcd0f564200e61d08f
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-09-25 21:16:23 +0100

    Make chemicaltable a server component.

--- src/app/client/chemicalsTableClient.tsx
+++ src/app/client/chemicalsTableClient.tsx
@@ -1,34 +1,72 @@
-"use client";
-import { useState } from "react";
+// import { useState } from "react";
 import { ChemicalRow } from "@/schemas/chemical";
-import { chemicalTableColumns } from "./chemicalTableColumns";
+import EditableStock from "./editableStock";
 
+type TableColumn<T, K extends keyof T = keyof T> = {
+    field: K;
+    label: string;
+    format?: (value: T) => React.ReactNode;
+};
+export const chemicalTableColumns: TableColumn<ChemicalRow>[] = [
+    { field: "name", label: "Name" },
+      {  field: "stockQuantity", label: "Stock", format: (c: ChemicalRow) => {
+            let displayVal;
+            switch (c.quantityType) {
+                case "MASS":
+                    displayVal = `${c.stockQuantity} ${c.unit ?? ""}`;
+                    break;
+                case "MASS":
+                    displayVal = `${c.stockQuantity} g`;
+                    break;
+                case "VOLUME":
+                    displayVal = `${c.stockQuantity} ml`;
+                    break;
+                default:
+                    displayVal = "";
+                    break;
+            }
+            return displayVal;
+        },
+      },
+      { field: "status", label: "Status" },
+    {
+        field: "supplier", label: "Supplier", format: (c: ChemicalRow) => {
+            return c.supplier.name;
+        }
+    },
+    {
+        field: "location", label: "Location", format: (c: ChemicalRow) => (c.location ? `${c.location.address} ${c.location.country}` : "-"
+        )
+    },
+    {
+        field: "hazardClass", label: "Hazard class", format: (c: ChemicalRow) => (
+            c.hazardClass.length ? c.hazardClass.map(({ classification }) => (classification)).join(", ") : <em>No classification</em>)
+    }
+];
 
-export default function ChemicalTable({ initialChems } : {initialChems: ChemicalRow[]}) {
-    const [chemicals, setChemicals] = useState<ChemicalRow[]>(initialChems);
+type ChemicalTableProps = {
+    initialChems: ChemicalRow[]
+}
+export default function ChemicalTable({ initialChems }: ChemicalTableProps) {
     return (<table className="table">
-            <thead className="table-dark">
-                <tr>
-                    {chemicalTableColumns.map((col) => (
-                        <td key={col.field}>{col.label}</td>
-                    ))
-
-                    }
+        <thead className="table-dark">
+            <tr>
+                {chemicalTableColumns.map((col) => (
+                    <td key={col.field}>{col.label}</td>
+                ))}
+            </tr>
+        </thead>
+        <tbody>
+            {initialChems.map((chemical: ChemicalRow) => (
+                <tr key={chemical.id}>
+                    {chemicalTableColumns.map(({ field, format }) => (
+                        <td key={field}>
+                            {format ? format(chemical) : chemical[field]?.toString()}
+                        </td>
+                    )
+                    )}
                 </tr>
-            </thead>
-            <tbody>
-                {
-                    chemicals.map((chemical : ChemicalRow) => (
-                        <tr key={chemical.id}>
-                            {chemicalTableColumns.map(({field, format})  => (
-                               <td key={field}>
-                                    {format ? format(chemical) : chemical[field]?.toString()}
-                                </td>
-                            )    
-                            )}  
-                        </tr>
-                    ))
-                }
-            </tbody>
-         </table>);
+            ))}
+        </tbody>
+    </table>);
 }
