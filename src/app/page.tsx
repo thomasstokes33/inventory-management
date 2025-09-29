24a305995d95ea9b4aaf0b29aa8ba77afcea25f4
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-09-29 14:21:43 +0100

    Create home dashboard.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,68 +2,87 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { ChemicalRow, chemicalSchema } from "@/schemas/chemical";
 import prisma from "@/lib/prisma";
-import ChemicalTable from "./client/chemicalsTableClient";
+import CountCard from "./components/countCard";
+import StatsCard from "./components/statsCard";
 
 
 
 export default async function Home() {
-    const chemicalsRaw = await prisma.chemical.findMany({
+    // Lastest created chemicals
+    const daysOfCreatedChemicals = 5;
+    const date = new Date();
+    date.setDate(date.getDay() - daysOfCreatedChemicals);
+    const chemicalsRecentRaw = await prisma.chemical.findMany({
         include: {
-            supplier: true,
-            location: true,
             hazardClass: true
+        },
+        where: {
+            updatedAt: { gte: date }
         }
     });
-    const chemicals: ChemicalRow[] = chemicalsRaw.map(chem => chemicalSchema.parse(chem));
+    const chemicalsRecent: ChemicalRow[] = chemicalsRecentRaw.map(chem => chemicalSchema.parse(chem));
+    const chemicalsCount = await prisma.chemical.count();
+    const supplierCount = await prisma.supplier.count();
     return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ChemicalTable initialChems={chemicals}/>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <CountCard title="Total Chemicals" count={chemicalsCount} />
+                    </div>
+                    <div className="col-sm-6">
+                        <CountCard title="Total Suppliers" count={supplierCount} />
+                    </div>
+                </div>
+                <StatsCard<ChemicalRow> title={`Latest Chemicals (${daysOfCreatedChemicals} days)`} items={chemicalsRecent} renderItem={item => {
+                    return `${item.name} - ${item.status} - ${item.updatedAt.toLocaleDateString()}`;
+                }
+                } />
+            </main>
+            <footer className={styles.footer}>
+                <a
+                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/file.svg"
+                        alt="File icon"
+                        width={16}
+                        height={16}
+                    />
+                    Learn
+                </a>
+                <a
+                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/window.svg"
+                        alt="Window icon"
+                        width={16}
+                        height={16}
+                    />
+                    Examples
+                </a>
+                <a
+                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <Image
+                        aria-hidden
+                        src="/globe.svg"
+                        alt="Globe icon"
+                        width={16}
+                        height={16}
+                    />
+                    Go to nextjs.org →
+                </a>
+            </footer>
+        </div>
+    );
 }
