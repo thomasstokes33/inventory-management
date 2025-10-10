96ae1431fcd2407a00909e5f34686ec91de50f14
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-10 16:23:51 +0100

    Tighten the /chemicals API
    Especially surrounding out of bounds IDs.

--- src/app/api/chemicals/route.ts
+++ src/app/api/chemicals/route.ts
@@ -1,54 +1,73 @@
 import prisma from "@/lib/prisma";
 import { chemicalSchema } from "@/schemas/chemical";
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import z from "zod";
 
 export async function GET() {
     const chemicals = await prisma.chemical.findMany();
-    return NextResponse.json(chemicals);
+    return NextResponse.json({ data: chemicals }, { status: 200 });
 }
-
-const chemicalCreationSchema = chemicalSchema.pick({ name: true, quantityType: true, status: true, materialType: true, unit: true }).partial({unit:true});
+type FormError = string;
+const chemicalCreationSchema = chemicalSchema.pick({ name: true, quantityType: true, status: true, materialType: true, unit: true }).partial({ unit: true });
+type ChemicalCreation = z.infer<typeof chemicalCreationSchema>;
 export async function PUT(request: Request) {
     let formData : FormData;
     try {
         formData = await request.formData();
     } catch {
-        return NextResponse.json({error: "Incorrect format"}, {status: 400});
+        return NextResponse.json({ error: "Incorrect format" }, { status: 400 });
     }
-    const newChemical: Record<string, string|string[]> = {};
+    const newChemical: Record<string, string | string[]> = parseFormData(formData);
+    const formHazardClassIds = formData.getAll("hazardClass");  // Each item is a hazard class ID number.
+    const { valid: formHazardClasses, errors: hazardErrors } = await validateHazardClasses(formHazardClassIds);
+    const { valid: parsedChemical, errors: parsingErrors } = validateChemical(newChemical);
+    const errors = [...hazardErrors, ...parsingErrors];
+    if (!parsedChemical || errors.length > 0) {
+        console.warn({errors});
+        return NextResponse.json({ errors }, { status: 400 });
+    }
+    const chemical = await prisma.chemical.create({ data: { ...parsedChemical, hazardClass: { connect: formHazardClasses } } });
+    return NextResponse.json({ success: chemical }, { status: 200 });
+}
+
+function validateChemical(newChemical: Record<string, string | string[]>): { valid: ChemicalCreation | null, errors: FormError[] } {
+    const validatedChemical = chemicalCreationSchema.safeParse(newChemical);
+    const errors: FormError[] = [];
+    if (validatedChemical.success) return { valid: validatedChemical.data, errors: [] };
+    errors.push(z.prettifyError(validatedChemical.error));
+    return { valid: null, errors: errors };
+}
+
+async function validateHazardClasses(formHazardClassIds: FormDataEntryValue[]): Promise<{ valid: { id: number }[], errors: FormError[] }> {
+    const errors: FormError[] = [];
+    const formHazardClasses: { id: number }[] = [];
+    const dbHazardClasses = await prisma.hazardClass.findMany({ select: { id: true } });
+    const validIds = new Set(dbHazardClasses.map(hc => hc.id)); // use Set for fast lookup
+    if (formHazardClassIds.length > validIds.size) {
+        errors.push("Too many hazard classes specificed.");
+        return { valid: [], errors: errors };
+    }
+    for (const hazardClass of formHazardClassIds) {
+        const hazardClassId = Number(hazardClass);
+        if (isNaN(hazardClassId)) {
+            errors.push("Invalid hazard class ID");
+        } else if (!validIds.has(hazardClassId)) {
+            errors.push("Invalid hazard class ID");
+        } else {
+            formHazardClasses.push({ id: Number(hazardClass) });
+        }
+    }
+    return { valid: formHazardClasses, errors: errors };
+}
+
+function parseFormData(formData: FormData): Record<string, string | string[]> {
+    const result: Record<string, string | string[]> = {};
     for (const pair of formData) {
         if (typeof pair[1] == "string" && pair[1].length > 0) {
-            newChemical[pair[0]] = pair[1];
+            result[pair[0]] = pair[1];
         } else {
             console.warn(`Skipping field: ${pair[0]}`);
         }
     }
-    const hazardClassIds = formData.getAll("hazardClass");
-    const hazardClasses : {id : number}[] = [];
-    let incorrectlyFormatedHazardClasses = false;
-    for (const hazardClass of hazardClassIds) {
-        const hazardClassId = Number(hazardClass);
-        if (isNaN(hazardClassId)) {
-            incorrectlyFormatedHazardClasses = true;
-            break;
-        }
-        hazardClasses.push({id: Number(hazardClass)});
-    }
-    const parsedChemical = chemicalCreationSchema.safeParse(newChemical);
-    let response: NextResponse;
-    if (!parsedChemical.success || incorrectlyFormatedHazardClasses) {
-        const errors = parsedChemical.error ? z.treeifyError(parsedChemical.error) : "Incorrect hazard classes";
-        response = NextResponse.json(
-            {
-                errors: errors,
-            }, { status: 400}
-        );
-    } else {
-        
-        const chemical = await prisma.chemical.create({ data: {...parsedChemical.data, hazardClass: {connect: hazardClasses}}});
-        response = NextResponse.json({success: chemical}, {status: 200});
-    }
-    return response;
-
+    return result;
 }
