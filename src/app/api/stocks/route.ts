b39d8c877ee0d8619770a30e40d8e4f800bb65f9
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-12 01:16:31 +0100

    Add stocks API for creation.

---
+++ src/app/api/stocks/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/prisma";
+import { stockSchema } from "@/schemas/stock";
+import { Stock } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/binary";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const stocks = await prisma.stock.findMany();
+    return NextResponse.json({ data: stocks }, { status: 200 });
+}
+
+const stockCreationSchema = stockSchema.pick({ locationId: true, chemicalId: true });
+export async function PUT(request: Request) {
+    let data;
+    try {
+        data = await request.json();
+    } catch {
+        return NextResponse.json({ error: "Incorrect format" }, { status: 400 });
+    }
+    const validatedStock = stockCreationSchema.safeParse(data);
+    if (validatedStock.error) return NextResponse.json({ error: "Invalid types" }, { status: 400 });
+    let stock : Stock;
+    try {
+        stock = await prisma.stock.create({
+            data: {
+                location: { connect: { id: validatedStock.data.locationId } },
+                chemical: { connect: { id: validatedStock.data.chemicalId } },
+                stockQuantity: 0
+            }
+        });
+        return NextResponse.json({ stock: stock }, { status: 200 });
+    } catch (err) {
+        console.log((err as PrismaClientKnownRequestError).message);
+        return NextResponse.json({error: "Invalid ID combination"}, {status: 400});
+    }
+}
