4cd9343db6b047661d30175d0b34264af7cb16d5
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-13 21:38:00 +0100

    Add stock archive panel.
    For when a chemical and location combination is no longer in use.

--- src/app/stock-management/page.tsx
+++ src/app/stock-management/page.tsx
@@ -2,16 +2,25 @@
 import prisma from "@/lib/prisma";
 import StockCreation from "./components/StockCreation";
 import { LocationRecord, locationSchema } from "@/schemas/location";
-
+import StockDeletion from "./components/StockDeletion";
+import { stockSchema } from "@/schemas/stock";
 export  default async function IssueReceipt({ }) {
     const handleGoodsIssueReceipt = async () => {
     };
     const rawLocations = await prisma.location.findMany();
+    const rawStock = await prisma.stock.findMany({ where: { archived: { equals: false } },
+        include: {
+            chemical: {include: {hazardClass: true}},
+            location: true
+        }
+    });
+    const stocks = rawStock.map((stock) => stockSchema.parse(stock));
     const locations : LocationRecord[] = rawLocations.map((loc) => locationSchema.parse(loc));
     return (<div className="container-lg mt-5">
         <div className="row">
             <div className="col-md-6">
-              <StockCreation locations={locations}/>
+                <StockCreation locations={locations}/>
+                <StockDeletion stock={stocks}/>
             </div>
             <div className="col-md-6">
                 <div className="card">
