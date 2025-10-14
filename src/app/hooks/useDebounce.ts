c7cda13511cfe6d8b5c6e1a9e50de23d0d6f8908
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-14 15:33:17 +0100

    Add debounce function hook.

--- src/app/hooks/useDebounce.ts
+++ src/app/hooks/useDebounce.ts
@@ -13,3 +13,18 @@ export default function useDebounce<T>(value: T, timeout: number) {
 
     return debouncedVal;
 }  
+
+
+export function useFuncDebounce<I, R >(func: (...args: I[]) => Promise<R[]>, delay: number) {
+    const timeout = useRef<number>(null);
+    return (...args: I[]) => new Promise<R[]>((resolve) => {
+        if (timeout.current) {
+            clearTimeout(timeout.current);
+        }
+        timeout.current = window.setTimeout(async () => {
+            const res = await func(...args);
+            console.log(res);
+            resolve(res);
+        }, delay);
+    });
+}
