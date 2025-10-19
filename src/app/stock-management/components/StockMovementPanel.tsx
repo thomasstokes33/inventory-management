9bbd06ececc9df098c1152589b4650b5174bd4aa
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-19 11:42:19 +0100

    Define chemical and location helpers (outside of component for efficiency). And define all of the necessary useState variables.

--- src/app/stock-management/components/StockMovementPanel.tsx
+++ src/app/stock-management/components/StockMovementPanel.tsx
@@ -20,30 +20,78 @@ import { toastifyFetch } from "@/lib/toastHelper";
 import { useRouter } from "next/navigation";
 type StockMovementOption<T> = T extends { id: number } ? { label: string, value: T["id"] } : { label: string, value: T };
 type StockMovementPanelProps = { suppliers: SupplierRecord[], stockCount: number };
+
+async function fetchStocks(searchParams: URLSearchParams) {
+    const res = await fetch(`${API_ROUTES.STOCKS}?${searchParams.toString()}`);
     if (!res.ok) return [];
-    else {
-        const data = await res.json();
-        const chemicals: MinimalChemical[] = data.chemicals;
-        return chemicals.map(item => ({ value: item.id, label: item.name }));
+    const { data: stocks }: { data: StockRecord[] } = await res.json();
+    return stocks;
+}
+function parseParam(id?: number) {
+    if (!id || isNaN(Number(id))) {
+        return "";
+    } else {
+        return String(id);
     }
+}
+
+async function fetchLocationFilteredChemicalOptions(inputLocation?: StockMovementOption<LocationRecord> | null): Promise<StockMovementOption<MinimalChemical>[]> {  // Moved outside as no state is used.
+    const parsedLocationId = parseParam(inputLocation?.value);
+    const params = new URLSearchParams();
+    params.set(VALID_STOCK_GET_PARAMS.locId, parsedLocationId);
+    params.set(VALID_STOCK_GET_PARAMS.distinctChem, String(true));
+    const stocks = await fetchStocks(params);
+    console.log(stocks);
+    return stocks.map(s => ({ value: s.chemicalId, label: s.chemical.name }));
 };
-export default function StockMovementPanel({suppliers} : StockMovementPanelProps) {
-    const [chem, setChem] = useState<null | StockMovementOption<ChemicalRecord>>();
-    const [loc, setLoc] = useState<null | StockMovementOption<LocationRecord>>();
-    const [movementType, setMovementType] = useState<null | StockMovementOption<MovementType>>();
-    const [costType, setCostType] = useState<null | StockMovementOption<CostType>>();
-    const [moveDate, setMoveDate] = useState<Date | null>();
-    const [supplier, setSupplier] = useState<null | StockMovementOption<number>>();
-    const debouncedFetch = useFuncDebounce<string, StockMovementOption<number>>(fetchChemicalOptions, 500);
-    const [filteredChemicalsOptions, setFilteredChemicalsOptions] = useState<[] | StockMovementOption<number>[]>([]);
-    const [filteredLocationsOptions, setFilteredLocationsOptions] = useState<[] | StockMovementOption<number>[]>([]);
-    const movementTypeOptions: Options<StockMovementOption<MovementType>> = Object.values(MovementType).map(val => ({ label: val.toLocaleLowerCase(), value: val }));
-    const supplierOptions: Options<StockMovementOption<number>> = suppliers.map(sup => ({value: sup.id, label: sup.name}));
-    const costTypeOptions: { [K in MovementType]: StockMovementOption<CostType>[] } = {
-        ISSUE: [{ value: "SELL", label: "sell" }, { value: "NONE", label: "use/discard" }],
-        RECEIPT: [{ value: "PURCHASE", label: "purchase" }],
-        TRANSFER: [{ value: "TRANSFER", label: "transfer" }]
-    };
+
+async function fetchChemicalFilteredLocationOptions(inputChemical?: StockMovementOption<MinimalChemical> | null): Promise<StockMovementOption<LocationRecord>[]> {
+    const parsedChemId = parseParam(inputChemical?.value);
+    const params = new URLSearchParams();
+    params.set(VALID_STOCK_GET_PARAMS.chemId, parsedChemId);
+    params.set(VALID_STOCK_GET_PARAMS.distinctLoc, String(true));
+    const stocks = await fetchStocks(params);
+    return stocks.map(s => ({ value: s.locationId, label: formatLocation(s.location) }));
+}
+async function fetchPermittedChemicalsOptions(inputChem: string): Promise<StockMovementOption<MinimalChemical>[]> {
+    const params = new URLSearchParams();
+    params.set(VALID_STOCK_GET_PARAMS.chemicalName, inputChem);
+    params.set(VALID_STOCK_GET_PARAMS.distinctChem, String(true));
+    const stocks = await fetchStocks(params);
+    return stocks.map(s => ({ value: s.id, label: s.chemical.name })); // uses stock id.
+}
+
+async function fetchPermittedLocationOptions(inputLocation: string): Promise<StockMovementOption<LocationRecord>[]> {
+    const params = new URLSearchParams();
+    params.set(VALID_STOCK_GET_PARAMS.locationName, inputLocation);
+    params.set(VALID_STOCK_GET_PARAMS.distinctLoc, String(true));
+    const stocks = await fetchStocks(params);
+    return stocks.map(s => ({ value: s.id, label: formatLocation(s.location) }));
+}
+
+const movementTypeOptions: Options<StockMovementOption<MovementType>> = Object.values(MovementType).map(val => ({ label: val.toLocaleLowerCase(), value: val }));
+const costTypeOptions: { [K in MovementType]: StockMovementOption<CostType>[] } = {
+    ISSUE: [{ value: "SELL", label: "sell" }],
+    RECEIPT: [{ value: "PURCHASE", label: "purchase" }],
+    TRANSFER_IN: [{ value: "TRANSFER", label: "N/A" }],
+    TRANSFER_OUT: [{ value: "TRANSFER", label: "transfer out" }],
+    DISCARD: [{ value: "NONE", label: "N/A" }],
+    RETURN: [{ value: "NONE", label: "N/A" }, { value: "TRANSFER", label: "Return cost" }],
+    PRODUCTION_OUTPUT: [{ value: "PRODUCE", label: "production" }],
+    PRODUCTION_USE: [{ value: "NONE", label: "N/A" }]
+
+};
+export default function StockMovementPanel({ suppliers, stockCount }: StockMovementPanelProps) {
+    const [chem, setChem] = useState<null | StockMovementOption<ChemicalRecord>>(null);
+    const [loc, setLoc] = useState<null | StockMovementOption<LocationRecord>>(null);
+    const [movementType, setMovementType] = useState<null | StockMovementOption<MovementType>>(null);
+    const [costType, setCostType] = useState<null | StockMovementOption<CostType>>(null);
+    const [moveDate, setMoveDate] = useState<Date | null>(null);
+    const [supplier, setSupplier] = useState<null | StockMovementOption<SupplierRecord>>(null);
+    const debouncedFetchChemicals = useFuncDebounce<string, StockMovementOption<MinimalChemical>>(fetchPermittedChemicalsOptions, 500);
+    const debouncedFetchLocations = useFuncDebounce<string, StockMovementOption<LocationRecord>>(fetchPermittedLocationOptions, 500);
+    const [filteredChemicalsOptions, setFilteredChemicalsOptions] = useState<null | StockMovementOption<MinimalChemical>[]>(null);
+    const [filteredLocationsOptions, setFilteredLocationsOptions] = useState<null | StockMovementOption<LocationRecord>[]>(null);
     const filteredCostTypeOptions = movementType ? costTypeOptions[movementType.value] : [];
     return (<div className="card">
         <div className="card-header">Goods issue/receipt</div>
