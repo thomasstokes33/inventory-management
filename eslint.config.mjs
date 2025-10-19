a8ed21228edeb986adb6e887eb5a946f2b7c860e
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-19 11:21:45 +0100

    Add additional type checking in tsconfig and eslint.
    For example, exhaustive switch statements.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,24 +6,33 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 console.log("filename " + __filename + " dirname " + __dirname);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-    rules: {
-        semi: "warn",
-        quotes: ["warn", "double"],
-        ...jsxA11y.flatConfigs.recommended.rules,
-    }
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("plugin:@typescript-eslint/recommended"),
+    {
+        ignores: [
+            "node_modules/**",
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+            "eslint.config.mjs"
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+        rules: {
+            semi: "warn",
+            quotes: ["warn", "double"],
+            "@typescript-eslint/switch-exhaustiveness-check": "error",
+            ...jsxA11y.flatConfigs.recommended.rules,
+        },
+        
     }
 ];
 export default eslintConfig;
