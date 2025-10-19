b8ecf1df14b4aaa53c6780e63b526539a7ec886e
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-19 11:07:16 +0100

    Make api routes more maintainable and add a helper for generating API responses.

--- src/lib/apiRoutes.ts
+++ src/lib/apiRoutes.ts
@@ -1,5 +1,14 @@
+import { NextResponse } from "next/server";
+import StatusCode from "status-code-enum";
+const API_PREFIX = "/api";
 export const API_ROUTES = {
-    STOCKS: "/api/stocks",
-    CHEMICALS: "api/chemicals",
-    
+    STOCKS: API_PREFIX + "/stocks",
+    CHEMICALS: API_PREFIX + "/chemicals",
+    STOCK_MOVEMENT: API_PREFIX + "/stock-movement"
 };
+
+
+export type ApiResponse<T> =  { error: string } | { data: T } | { errors: string[] };
+export function generateResponse<T>(body: ApiResponse<T>, status: StatusCode) : NextResponse<ApiResponse<T>>{
+    return NextResponse.json(body, { status: status });
+}
