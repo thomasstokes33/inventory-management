bcd4120a9c5cacfbea42c8ee1d4ea6aca711ae6e
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-20 13:57:21 +0100

    Add ID checker to api routes

--- src/lib/apiRoutes.ts
+++ src/lib/apiRoutes.ts
@@ -12,3 +12,12 @@ export type ApiResponse<T> =  { error: string } | { data: T } | { errors: string
 export function generateResponse<T>(body: ApiResponse<T>, status: StatusCode) : NextResponse<ApiResponse<T>>{
     return NextResponse.json(body, { status: status });
 }
+
+export function validId(rawId: unknown)  : boolean {
+        // Handle zero length string, otherwise the chem ID becomes zero.
+    if (rawId === "") return false;
+    // Check ID is a number (but rely on try catch for ID validity.)
+    const chemId = Number(rawId);
+    if (isNaN(chemId)) return false;
+    return true;
+}
