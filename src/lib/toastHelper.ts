e11061ed7b8d959b6e217ec25295bbd3f044e4be
T Stokes - uni <thomasstokes@yahoo.co.uk>
2025-10-23 17:20:46 +0100

    Prepare for ability to toast the JSON error.

--- src/lib/toastHelper.ts
+++ src/lib/toastHelper.ts
@@ -4,15 +4,22 @@ async function checkResponse(response: Response) {
     if (response.ok) {
         return response.json();
     } else {
+        // let msg;
+        // try {
+        //     const val = await response.json();
+        //     msg = val.error ?? "";
+        // } catch {
+        //     msg = response.statusText;
+        // }
         throw new Error(response.statusText);
     }
 };
 
-export default function toastify<T>(promise: Promise<T>, messages: { loading: string, success: string, error: string }, successHandler: (val: T) => void, failureHandler: (val: T) => void) {
+export default function toastify<T>(promise: Promise<T>, messages: { loading: string, success: string, error: string | ((err: Error) => string) }, successHandler: (val: T) => void, failureHandler: (val: T) => void) {
     toast.promise(promise, messages).then(successHandler).catch(failureHandler);
 }
 
-export function toastifyFetch<T>(url : string, options: RequestInit, messages: { loading: string, success: string, error: string }, successHandler: (val: T) => void, failureHandler: (val: T) => void) {
+export function toastifyFetch<T>(url : string, options: RequestInit, messages: { loading: string, success: string, error: string | ((err: Error) => string) }, successHandler: (val: T) => void, failureHandler: (val: T) => void) {
     const fetchPromise = fetch(url, options).then(checkResponse);
     toastify<T>(fetchPromise, messages, successHandler, failureHandler);
 }
